b3d65d3412d518912e9bf6cfe1fe7a4471aad054
Matt <45330248+MasterMatt7@users.noreply.github.com>
2020-06-09 09:45:19 +1200

    Update robot.cpp
    Changed the array to a vector so that elements can be added each time the image for loop iterates through the pixels.

--- robot.cpp
+++ robot.cpp
@@ -16,7 +16,7 @@ int main(){
 		double vLeft = 5.0;
 		double vRight = 5.0;
 		double d = 1.1; //placeholder constant for turning
-		int* whiteLine = new int[3]; //white pixel horizontal index
+		std::vector<int> whiteLine; //white pixel horizontal index
 	    
 		for (int i = 0; i < 150; i++) { // image is 150px wide
 			int pix = get_pixel(cameraView, 50, i, 3); // 50 is in the middle of the camera view
@@ -33,8 +33,9 @@ int main(){
 			
 			if (isWhite == 1){
 				//std::cout << i <<std::endl;
-				Whiteline[0] = i;   //Need to figrue out to append the 4 whitepixel indexs to the whiteLine array
+				whiteLine.push_back(i); //Adds each white pixel index to the vector 
 		}
+		// There are 4 white pixels and to be in the centre they need to be at 73,74,75,76.
 		if (whiteLine[0] < 73){    //white line moving to the left
 			vRight = vRight * d; //turn robot left
 		}
