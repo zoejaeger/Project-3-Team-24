2666edb8190d2f9834cc7c32d80d3f762fb87601
Matt <45330248+MasterMatt7@users.noreply.github.com>
2020-06-18 13:47:41 +1200

    Update pixel_scanners.hpp
    Removed cout statements used for testing.

--- pixel_scanners.hpp
+++ pixel_scanners.hpp
@@ -11,11 +11,9 @@ void redScanner(int column, std::vector<int> &redPixScanner){ // detects red pix
 			} else {
 				isRed = 0;
 			} 
-			//std::cout<<isRed<<"";
 			
 			if (isRed == 1) {
-				std::cout<<  i <<std::endl;
-				redPixScanner.push_back(i); //Adds each red pixel index to the redWall vector
+				redPixScanner.push_back(i); //Adds each red pixel index to the vector
 			} 
 			
 		}
@@ -23,18 +21,15 @@ void redScanner(int column, std::vector<int> &redPixScanner){ // detects red pix
 
 void whiteScanner(int column, std::vector<int> &whitePixScanner){ // detects white pixels and then adds their horizontal index to a vector
 	for (int i = 0; i < 150; i++) { // image is 150px wide
-			int pix = get_pixel(cameraView, column, i, 3); // 50 is in the middle of the camera view
+			int pix = get_pixel(cameraView, column, i, 3);
 			
 			int isWhite;
-			int whiteIndex;
 			
 			if (pix > 250) {
 				isWhite = 1;
 			} else {
 				isWhite = 0;
 			} 
-			std::cout<<isWhite<<" ";
-			
 			if (isWhite == 1) {
 				whitePixScanner.push_back(i); //Adds each white pixel index to the vector
 			}
